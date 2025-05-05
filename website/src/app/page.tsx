768ea90e748e2c90072b18d90107451210f76302
robinnicholas <robin.raja@anywhere.co>
2025-05-05 23:26:20 +0530

    feat: button component added

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,11 +1,21 @@
 import { Applied, FullTimne, Remote } from "./components/Badge";
+import Button from "./components/Button";
 
 export default function Home() {
   return (
+    <>
     <div className="flex gap-2 mx-40 my-20">
+      <h2>Badges</h2>
       <FullTimne />
       <Remote />
       <Applied />
     </div>
+    <div className="flex gap-2 mx-40 my-20">
+      <h2>Buttons</h2>
+      <Button size="small">Get Curated Jobs</Button>
+      <Button size="medium">Get Curated Jobs</Button>
+      <Button size="large">Get Curated Jobs</Button>
+    </div>
+    </>
   );
 }
