7f60f69b44dffee87ff3977f1db644e3be35ca09
robinnicholas <robin.raja@anywhere.co>
2025-05-10 13:55:48 +0530

    feat: added Testimonial

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -3,7 +3,54 @@ import Button from "../components/Button";
 import { Container } from "../components/Container";
 import Header from "../components/Layout/Header";
 import AboutSection from "../components/Layout/AboutSection";
+import TestimonialCard from "../components/TestimonialCard";
 
+const testimonials = [
+  {
+    companyImage: {
+      src: "caribou.svg",
+      alt: "Caribou Logo",
+    },
+    socialImage: {
+      src: "linkedin.svg",
+      alt: "Linkedin Logo",
+    },
+    description: "I was skeptical at first, but this job platform exceeded my expectations. The AI-driven job matching system is incredibly accurate, helping me find roles that truly fit my experience and career ambitions. What I love most is how easy it is to apply directly and track my applications."
+  },
+  {
+    companyImage: {
+      src: "caribou.svg",
+      alt: "Caribou Logo",
+    },
+    socialImage: {
+      src: "x.svg",
+      alt: "X Logo",
+    },
+    description: "This platform completely changed my job search! I found the perfect role in just a few weeks. Highly recommended!"
+  },
+  {
+    companyImage: {
+      src: "caribou.svg",
+      alt: "Caribou Logo",
+    },
+    socialImage: {
+      src: "linkedin.svg",
+      alt: "Linkedin Logo",
+    },
+    description: "I was skeptical at first, but this job platform exceeded my expectations. The AI-driven job matching system is incredibly accurate, helping me find roles that truly fit my experience and career ambitions. What I love most is how easy it is to apply directly and track my applications."
+  },
+  {
+    companyImage: {
+      src: "caribou.svg",
+      alt: "Caribou Logo",
+    },
+    socialImage: {
+      src: "x.svg",
+      alt: "X Logo",
+    },
+    description: "This platform completely changed my job search! I found the perfect role in just a few weeks. Highly recommended!"
+  }
+]
 export default function Home() {
   return (
     <>
@@ -27,12 +74,48 @@ export default function Home() {
         </Container>
       </section>
       <AboutSection />
-      <section className="pb-25 md:pb-32 lg:pb-40">
-        <Container className="flex flex-col items-center">
-          <OutlineBadge className="mb-4">Testimonials</OutlineBadge>
-          <p className="font-ibm-plex-mono font-bold text-lg mb-9">See why our users love axtionable.</p>
+      <section className="relative pb-25 md:pb-32 lg:pb-40">
+        <div className="absolute inset-y-0 right-0 left-[90%] bg-linear-to-r to-white from-white/0 z-10"></div>
+        <div className="absolute inset-y-0 right-[90%] left-0 bg-linear-to-r to-white/0 from-white z-10"></div>
+        <Container>
+          <div className="flex flex-col items-center">
+            <OutlineBadge className="mb-4">Testimonials</OutlineBadge>
+            <p className="font-ibm-plex-mono font-bold text-lg">See why our users love axtionable.</p>
+          </div>
+          </Container>
+          <div className="overflow-x-hidden my-8 md:my-16">
+            <div className="flex gap-9">
+              <ul className="animate-marquee flex flex-row shrink-0 gap-9 justify-between">
+                {testimonials.map((testimonial, idx) => {
+                  return (
+                    <li key={idx} className="shrink-0">
+                      <TestimonialCard
+                        companyImage={testimonial.companyImage}
+                        socialImage={testimonial.socialImage}
+                        description={testimonial.description}
+                      />
+                    </li>
+                  )
+                })}
+              </ul>
+              <ul className="animate-marquee flex flex-row shrink-0 gap-9 justify-between">
+                {testimonials.map((testimonial, idx) => {
+                  return (
+                    <li key={idx} className="shrink-0">
+                      <TestimonialCard
+                        companyImage={testimonial.companyImage}
+                        socialImage={testimonial.socialImage}
+                        description={testimonial.description}
+                      />
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          </div>
+          <Container>
           <div className="flex justify-center">
-            <Button className="mt-4 h-8 px-4 md:h-14 md:px-6 lg:h-20 lg:px-8">Get Curated Jobs</Button>
+            <Button className="h-8 px-4 md:h-14 md:px-6">Get Curated Jobs</Button>
           </div>
         </Container>
       </section>
